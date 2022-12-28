5ce46708f1988d75f7cf1d310faee66b37dfa7b1
123wwwa <123wwa@naver.com>
2022-12-28 09:38:16 +0900

    first commit

---
+++ src/main/java/com/example/demo/HexaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaBackendApplication.class, args);
+	}
+
+}
