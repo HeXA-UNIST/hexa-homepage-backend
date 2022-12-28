b948bd82ac99c8f6ca1f7f0a93d0ab89292ba31e
SYChoi <sychoi@help-me.kr>
2022-12-28 13:49:06 +0900

    init setting

---
+++ hexa-homepage-backend-main-app/src/main/java/pro/hexa/backend/main/api/domain/sample/controller/SampleController.java
@@ -0,0 +1,8 @@
+package pro.hexa.backend.main.api.domain.sample.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+}
