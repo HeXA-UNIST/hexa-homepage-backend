6d280b33146981ecec7d5378bc5e6f4ae4ec2219
SYChoi <sychoi@help-me.kr>
2023-01-05 19:10:22 +0900

    bug fixed

--- hexa-homepage-backend-domain/src/main/java/pro/hexa/backend/domain/news/repository/NewsRepository.java
+++ hexa-homepage-backend-domain/src/main/java/pro/hexa/backend/domain/news/repository/NewsRepository.java
@@ -1,8 +1,10 @@
 package pro.hexa.backend.domain.news.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pro.hexa.backend.domain.news.domain.News;
 
-public interface NewsRepository extends JpaRepository<Long, News>, NewsRepositoryCustom {
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long>, NewsRepositoryCustom {
 
 }
