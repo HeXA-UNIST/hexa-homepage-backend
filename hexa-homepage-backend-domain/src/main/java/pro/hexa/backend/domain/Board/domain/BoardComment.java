1e5a5a3af340ee59d91047ec44b7ad16d9acd84a
eumppe <pych05@unist.ac.kr>
2023-02-07 23:38:30 +0900

    william

--- hexa-homepage-backend-domain/src/main/java/pro/hexa/backend/domain/Board/domain/BoardComment.java
+++ hexa-homepage-backend-domain/src/main/java/pro/hexa/backend/domain/Board/domain/BoardComment.java
@@ -2,13 +2,20 @@ package pro.hexa.backend.domain.Board.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.Comment;
+
+import lombok.Getter;
 import pro.hexa.backend.domain.model.model.AbstractEntity;
+import pro.hexa.backend.domain.user.domain.User;
 
 @Entity
+@Getter
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class BoardComment extends AbstractEntity {
 
@@ -17,7 +24,8 @@ public abstract class BoardComment extends AbstractEntity {
     private String content;
 
     @Comment(value = "작성자")
-    @Column
-    private int writerUser;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id")
+    private User writerUser;
 
 }
