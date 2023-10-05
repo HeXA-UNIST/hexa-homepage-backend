bf432971175115841ea201efd1ad62e3de3f13bd
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-10-05 13:40:07 +0900

    newsRepositoryImpl 주석 추가

--- hexa-homepage-backend-domain/src/main/java/pro/hexa/backend/domain/news/repository/NewsRepositoryImpl.java
+++ hexa-homepage-backend-domain/src/main/java/pro/hexa/backend/domain/news/repository/NewsRepositoryImpl.java
@@ -25,6 +25,9 @@ public class NewsRepositoryImpl implements NewsRepositoryCustom {
             .fetch();
     }
 
+    /*  News 리스트 조회를 위한 메서드 (페이징 처리)
+    *  1. input : pageNum - 보여줄 페이지 쪽수, perPage - 한 페이지당 보여줄 항목 수
+    *  2. output : news List 리턴 (pageNum 쪽의 perPage 갯수 만큼) */
     @Override
     public List<News> findAllWithPaging(Integer pageNum, Integer perPage) {
         QNews news = QNews.news;
@@ -50,6 +53,9 @@ public class NewsRepositoryImpl implements NewsRepositoryCustom {
         );
     }
 
+    /*  전체 페이지 수를 리턴하기 위한 메서드
+    *  1. input : perPage - 한 페이지당 갯수
+    *  2. output : 전체 페이지 수 */
     @Override
     public int getMaxPage(Integer perPage) {
         QNews news = QNews.news;
@@ -60,6 +66,6 @@ public class NewsRepositoryImpl implements NewsRepositoryCustom {
         return Math.toIntExact(queryFactory.select(news.id.count())
                 .from(news)
                 .where(whereQuery)
-                .fetchFirst()) / perPage;
+                .fetchFirst()) / perPage;  // 전체 페이지 수 = 전체 데이터 갯수 / 한 페이지 당 항목 수 perPage
     }
 }
