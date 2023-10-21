0eb3cf8a62f62959bc2dfb387575b5ed3ea64fae
sychoi <sychoi@standby.kr>
2023-10-21 16:24:42 +0900

    reformat code & delete system.out

--- hexa-homepage-backend-main-app/src/test/java/pro/hexa/backend/main/api/domain/main/controller/MainPageControllerTest.java
+++ hexa-homepage-backend-main-app/src/test/java/pro/hexa/backend/main/api/domain/main/controller/MainPageControllerTest.java
@@ -2,15 +2,16 @@ package pro.hexa.backend.main.api.domain.main.controller;
 
 
 import java.time.LocalDate;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import pro.hexa.backend.domain.news.domain.News;
-import static pro.hexa.backend.domain.news.model.NEWS_TYPE.공지;
 import pro.hexa.backend.domain.news.repository.NewsRepository;
 import pro.hexa.backend.main.api.domain.main.dto.MainPageResponse;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static pro.hexa.backend.domain.news.model.NEWS_TYPE.공지;
 
 @SpringBootTest
 class MainPageControllerTest {
@@ -25,20 +26,19 @@ class MainPageControllerTest {
     @Test
     void getMainPageResponse() {
         News news1 = News.create(
-                127L,
-                공지,
-                "title",
-                LocalDate.of(2023, 1,1),
-                "some"
+            127L,
+            공지,
+            "title",
+            LocalDate.of(2023, 1, 1),
+            "some"
         );
 
         newsRepository.save(news1);
 
-        ResponseEntity<MainPageResponse> response=mainPageController.getMainPageResponse();
+        ResponseEntity<MainPageResponse> response = mainPageController.getMainPageResponse();
 
         MainPageResponse mainPageResponse = response.getBody();
         assertNotNull(mainPageResponse);
-        System.out.println(mainPageResponse.getNewsList());
 
     }
 }
